dd8d0fe4f59db2826b0ad7ce5a53eda663036933
EugenieKurkova <55740376+EugenieKurkova@users.noreply.github.com>
2019-09-26 13:45:53 +0300

    update test_items.py
    adding code

--- test_items.py
+++ test_items.py
@@ -1,1 +1,7 @@
+link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+def test_find_add_to_basket_button(parametrized_browser):
+    parametrized_browser.implicitly_wait(25)
+    parametrized_browser.get(link)
+    assert parametrized_browser.find_element_by_class_name("btn-add-to-basket").is_displayed(), \
+    "add-to-basket-button is not found"
